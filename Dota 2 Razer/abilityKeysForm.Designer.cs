f81b543baab3df22154e3e27036bf3bef6893bc3
Fernando Mendez <fernandomendez101@gmail.com>
2016-02-10 18:20:00 -0500

    Initial Push

---
+++ Dota 2 Razer/abilityKeysForm.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Dota_2_Razer
+{
+    partial class AbilityKeysForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.skill1Button = new System.Windows.Forms.Button();
+            this.skill2Button = new System.Windows.Forms.Button();
+            this.skill3Button = new System.Windows.Forms.Button();
+            this.skill4Button = new System.Windows.Forms.Button();
+            this.skill5Button = new System.Windows.Forms.Button();
+            this.skill6Button = new System.Windows.Forms.Button();
+            this.saveKeysButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // skill1Button
+            // 
+            this.skill1Button.Location = new System.Drawing.Point(13, 13);
+            this.skill1Button.Name = "skill1Button";
+            this.skill1Button.Size = new System.Drawing.Size(75, 67);
+            this.skill1Button.TabIndex = 0;
+            this.skill1Button.Text = "Q";
+            this.skill1Button.UseVisualStyleBackColor = true;
+            this.skill1Button.Click += new System.EventHandler(this.skill1Button_Click);
+            this.skill1Button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.skill1Button_KeyPress);
+            // 
+            // skill2Button
+            // 
+            this.skill2Button.Location = new System.Drawing.Point(94, 13);
+            this.skill2Button.Name = "skill2Button";
+            this.skill2Button.Size = new System.Drawing.Size(75, 67);
+            this.skill2Button.TabIndex = 1;
+            this.skill2Button.Text = "W";
+            this.skill2Button.UseVisualStyleBackColor = true;
+            this.skill2Button.Click += new System.EventHandler(this.skill2Button_Click);
+            this.skill2Button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.skill2Button_KeyPress);
+            // 
+            // skill3Button
+            // 
+            this.skill3Button.Location = new System.Drawing.Point(175, 12);
+            this.skill3Button.Name = "skill3Button";
+            this.skill3Button.Size = new System.Drawing.Size(75, 67);
+            this.skill3Button.TabIndex = 2;
+            this.skill3Button.Text = "E";
+            this.skill3Button.UseVisualStyleBackColor = true;
+            this.skill3Button.Click += new System.EventHandler(this.skill3Button_Click);
+            this.skill3Button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.skill3Button_KeyPress);
+            // 
+            // skill4Button
+            // 
+            this.skill4Button.Location = new System.Drawing.Point(256, 12);
+            this.skill4Button.Name = "skill4Button";
+            this.skill4Button.Size = new System.Drawing.Size(75, 67);
+            this.skill4Button.TabIndex = 3;
+            this.skill4Button.Text = "R";
+            this.skill4Button.UseVisualStyleBackColor = true;
+            this.skill4Button.Click += new System.EventHandler(this.skill4Button_Click);
+            this.skill4Button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.skill4Button_KeyPress);
+            // 
+            // skill5Button
+            // 
+            this.skill5Button.Location = new System.Drawing.Point(337, 12);
+            this.skill5Button.Name = "skill5Button";
+            this.skill5Button.Size = new System.Drawing.Size(75, 67);
+            this.skill5Button.TabIndex = 4;
+            this.skill5Button.Text = "T";
+            this.skill5Button.UseVisualStyleBackColor = true;
+            this.skill5Button.Click += new System.EventHandler(this.skill5Button_Click);
+            this.skill5Button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.skill5Button_KeyPress);
+            // 
+            // skill6Button
+            // 
+            this.skill6Button.Location = new System.Drawing.Point(418, 12);
+            this.skill6Button.Name = "skill6Button";
+            this.skill6Button.Size = new System.Drawing.Size(75, 67);
+            this.skill6Button.TabIndex = 5;
+            this.skill6Button.Text = "F";
+            this.skill6Button.UseVisualStyleBackColor = true;
+            this.skill6Button.Click += new System.EventHandler(this.skill6Button_Click);
+            this.skill6Button.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.skill6Button_KeyPress);
+            // 
+            // saveKeysButton
+            // 
+            this.saveKeysButton.Location = new System.Drawing.Point(13, 110);
+            this.saveKeysButton.Name = "saveKeysButton";
+            this.saveKeysButton.Size = new System.Drawing.Size(480, 23);
+            this.saveKeysButton.TabIndex = 6;
+            this.saveKeysButton.Text = "Save Keys";
+            this.saveKeysButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 84);
+            this.textBox1.MaxLength = 6;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(52, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "00FFBB";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Normal Color";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(143, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Cooldown Color";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(230, 84);
+            this.textBox2.MaxLength = 6;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(52, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "FF0000";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(288, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Silenced Color";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(369, 85);
+            this.textBox3.MaxLength = 6;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(52, 20);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Text = "FF00FF";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // AbilityKeysForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 145);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.saveKeysButton);
+            this.Controls.Add(this.skill6Button);
+            this.Controls.Add(this.skill5Button);
+            this.Controls.Add(this.skill4Button);
+            this.Controls.Add(this.skill3Button);
+            this.Controls.Add(this.skill2Button);
+            this.Controls.Add(this.skill1Button);
+            this.Name = "AbilityKeysForm";
+            this.Text = "Change Ability Keys";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button skill1Button;
+        private System.Windows.Forms.Button skill2Button;
+        private System.Windows.Forms.Button skill3Button;
+        private System.Windows.Forms.Button skill4Button;
+        private System.Windows.Forms.Button skill5Button;
+        private System.Windows.Forms.Button skill6Button;
+        private System.Windows.Forms.Button saveKeysButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
